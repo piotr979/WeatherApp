ae70d0d57e34296251dc2dd9e18a5817f658f745
Piotr Glaza <daftu44@gmail.com>
2019-01-31 10:47:46 +0000

    Horizontal stack views

---
+++ Weather 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_graphics2
+//
+//  Created by Piotr Glaza on 29/01/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
