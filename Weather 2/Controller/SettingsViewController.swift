82955d085ac1f56574e734b864cb0bd0ee73cce0
Piotr Glaza <daftu44@gmail.com>
2019-02-24 16:33:10 +0000

    After splitting VC

---
+++ Weather 2/Controller/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Weather v2
+//
+//  Created by Piotr Glaza on 19/02/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather 2/Model/WeatherDataManagement.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataManagement.swift
+//  Weather v2
+//
+//  Created by Piotr Glaza on 24/02/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import Foundation
